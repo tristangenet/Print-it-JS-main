0ca80421aaeb174a22e70c7046a819d884d1c6cd
Tristan <tgenetpro@gmail.com>
2023-11-20 09:55:14 +0100

    flèches interactives
    Mise en place des flèches interactives

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,36 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionnez vos éléments HTML
+    var banner = document.getElementById('banner');
+    var arrowLeft = document.querySelector('.arrow_left');
+    var arrowRight = document.querySelector('.arrow_right');
+
+    // Définissez une variable pour suivre l'index de la diapositive actuelle
+    var currentSlideIndex = 0;
+
+    // Fonction pour afficher la diapositive en fonction de l'index
+    function showSlide(index) {
+        var slide = slides[index];
+        var image = slide.image;
+        var tagLine = slide.tagLine;
+
+        // Mettez à jour l'image et le texte dans le HTML
+        document.querySelector('.banner-img').src = './assets/images/slideshow/' + image;
+        document.querySelector('p').innerHTML = tagLine;
+    }
+
+    // Ajoutez un écouteur d'événements pour la flèche gauche
+    arrowLeft.addEventListener('click', function () {
+        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+        showSlide(currentSlideIndex);
+    });
+
+    // Ajoutez un écouteur d'événements pour la flèche droite
+    arrowRight.addEventListener('click', function () {
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+        showSlide(currentSlideIndex);
+    });
+});
